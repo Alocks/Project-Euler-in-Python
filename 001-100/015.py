f2724299d77810b427e002b80ea505a89d7efef5
Alocks <alocksmasao@gmail.com>
2019-04-23 15:28:11 -0300

    Delete 015.py

--- 001-100/015.py
+++
@@ -1,6 +0,0 @@
-#Starting in the top left corner of a 2×2 grid, and only beingable to move
-#to the right and down, there are exactly 6 routes to the bottom right corner.
-#How many such routes are there through a 20×20 grid?
-import math
-x,y=12,8
-print(math.factorial(x+y)/math.factorial(10)**2)
