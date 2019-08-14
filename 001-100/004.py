c92ab74b7272ea1ee608d06769b5a487ca1d39ae
Alocks <alocksmasao@gmail.com>
2019-08-14 15:24:55 -0300

    Update 004.py

--- 001-100/004.py
+++ 001-100/004.py
@@ -1,21 +1,20 @@
 #A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
-def maxPolindrom(value):
-    arr = []
-    x = (len(str(value)))
-    if (x > 1):
-        x = int(('9')*(x-1))
-    else:
-        x = 9
-           
-    for i in range(value, x , -1):
-        for j in range(value, x, -1):
-            res = i * j
-            if str(res) == str(res)[::-1]:
-                arr.append(res)
-    maxPoli = max(arr)
-    print(arr)
-    return maxPoli
+def is_pali(value):
+    return str(value) == str(value)[::-1]
 
-print(maxPolindrom(999))
+limit_x=999
+limit_y=100
+array=[]
+
+while(True):
+    for i in range(limit_x, limit_y, -1):
+        for j in range(limit_x, limit_y, -1):
+            tot=i*j
+            if is_pali(tot):
+                array.append(tot)
+                break
+    break
+
+print(max(array))
